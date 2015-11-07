a7929430d203a988b4b3a37e0cf875eebc3e20c5
Rafael Marques Rodrigues <rafael.marques.rodrigues95@gmail.com>
2015-11-07 12:39:07 -0200

    first version of the dtw algorithm

--- main.py
+++ main.py
@@ -1,61 +1,77 @@
 
+import operator
+import sys
+
 test_filename = 'test.in'
 training_filename = 'training.in'
 label_filename = 'label.in'
 
-
-def readFile(filename):
+#creates a dictionary with 'class': [numbers of the series] or 'class': name of the class, if its used to read the label
+def readFile(filename, function):
     d = {}
     
     with open(filename) as f:
         for line in f:
             content = line.split()
-            d[int(content[0])] = [float(x) for x in content]
+            d[int(content[0])] = function(content)
 
     return d
 
-def getLabel(filename):
+def getFloatContent(content):
+    return [float(x) for x in content]
+
+def getStrContent(content):
+    return content[1]
+
+def dtw(test, training):
+    #start the matrix with max float values
+    values = [[sys.float_info.max for i in range(0, len(training) + 1)] for j in range(0, len(test) + 1)]
+    
+    #(0, 0) position starts with 0
+    values[0][0] = 0.0
+    
+    #calculate the value for each position on the matrix
+    for i in range(1, len(test)):
+        for j in range(1, len(training)):
+            values[i][j] = (test[i] - training[j]) * (test[i] - training[j]) + min(values[i-1][j-1], values[i][j-1], values[i-1][j])
+    
+    #return the dtw value accordingly to the size of the series
+    return values[len(test)-1][len(training)-1]
+        
+
+#####################################################################################
+#                       CALCULATING MOVEMENTS
+#####################################################################################
+
+#read files
+test_data = readFile(test_filename, getFloatContent)
+training_data = readFile(training_filename, getFloatContent)
+label = readFile(label_filename, getStrContent)
+
+#get list of keys
+test_keys = test_data.keys()
+training_keys = training_data.keys()
+
+hits = 0
+
+#for each test key calculate dtw and compare with the training key
+for test_key in test_keys:
     d = {}
     
-    with open(filename) as f:
-        for line in f:
-            content = line.split()
-            d[int(content[0])] = content[1]
-
-    return d
-            
-def printDict(d):
-    keys = d.keys()
-
-    for key in keys:
-        print(key, d[key])
-
-def dtw(test, training, i, j):
-    if i < 0 or j < 0:
-        return 0
+    #calculating dtw for each training series with the actual test series
+    for training_key in training_keys:
+        d[training_key] = dtw(test_data[test_key], training_data[training_key])
     
-    value = (float(test[i]) - float(training[j])) * (float(test[i]) - float(training[j]))
+    #sort all the dtw calculated
+    results = sorted(d.items(), key=operator.itemgetter(1))
     
-    print(i, j)
+    #get the best result from them
+    print("DTW:", results[0][1], "Class:", label[results[0][0]])
+    
+    #add a hit if correct
+    if results[0][0] == test_key:
+        hits = hits + 1
 
-    return value + min(dtw(test, training, i-1, j-1), dtw(test, training, i, j-1), dtw(test, training, i-1, j))
-
-
-test_data = readFile(test_filename)
-training_data = readFile(training_filename)
-label = getLabel(label_filename)
-
-#printDict(test_data)
-#printDict(training_data)
-#printDict(label)
-
-keysA = list(test_data.keys())
-keysB = list(training_data.keys())
-
-a = test_data[keysA[0]]
-b = training_data[keysB[0]]
-
-print(dtw(a, b, len(a)-1, len(b)-1))
-
+print("Accuracy:", hits/len(test_keys))
 
 
