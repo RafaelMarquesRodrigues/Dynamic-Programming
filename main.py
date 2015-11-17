2575ac794139215f80d129f366805ece45d1ff61
Rafael Marques Rodrigues <rafael.marques.rodrigues95@gmail.com>
2015-11-17 20:28:34 -0200

    changed names wtf

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ def getFloatContent(content):
 def getStrContent(content):
     return content[1]
 
-def dtw(test, training):
+def calculateDtw(test, training):
     #start the matrix with max float values
     values = [[sys.float_info.max for i in range(0, len(training) + 1)] for j in range(0, len(test) + 1)]
-    
+   
     #(0, 0) position starts with 0
     values[0][0] = 0.0
     
@@ -39,7 +39,7 @@ def dtw(test, training):
 #                       CALCULATING MOVEMENTS
 #####################################################################################
 
-def calculateDtw(title, test_filename, training_filename, label_filename):
+def processFiles(title, test_filename, training_filename, label_filename):
     print("**************", title, "**************", sep='\n')
 
     #read files
@@ -59,7 +59,7 @@ def calculateDtw(title, test_filename, training_filename, label_filename):
         
         #calculating dtw for each training series with the actual test series
         for training_key in training_keys:
-            d[training_key] = dtw(test_data[test_key], training_data[training_key])
+            d[training_key] = calculateDtw(test_data[test_key], training_data[training_key])
         
         #sort all the dtw calculated
         results = sorted(d.items(), key=operator.itemgetter(1))
@@ -76,8 +76,8 @@ def calculateDtw(title, test_filename, training_filename, label_filename):
 
     print("Accuracy:", hits/len(test_keys))
 
-calculateDtw("Rotulos 1D", "test.in", "training.in", "label.in")
+processFiles("Rotulos 1D", "test.in", "training.in", "label.in")
 
 #precisa fazer a extensão pra calcular esse
-#calculateDtw("Rotulos 3D", "test3D.in", "training3D.in", "label3D.in")
+#processFiles("Rotulos 3D", "test3D.in", "training3D.in", "label3D.in")
 
